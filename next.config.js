fa684594325e90dae602c2e3f935697d93ed0e63
Samuel Ige <samuelige.iyanu@gmail.com>
2023-10-09 22:59:24 +0100

    add - image domain to next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    images: {
+        domains: ["samuelige.github.io/skilled-elearning-nextjs"],
+    },
 }
 
 module.exports = nextConfig
